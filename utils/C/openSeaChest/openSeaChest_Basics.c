3b07f3847345adf9250631f90a665e2ecb90b49a
Tyler Erickson <tyler.j.erickson@seagate.com>
2019-11-08 14:04:44 -0700

    Updating to new hacks location for SAT 12B CDBs

--- utils/C/openSeaChest/openSeaChest_Basics.c
+++ utils/C/openSeaChest/openSeaChest_Basics.c
@@ -952,7 +952,7 @@ int32_t main(int argc, char *argv[])
         if (SAT_12_BYTE_CDBS_FLAG)
         {
             //set SAT12 for this device if requested
-            deviceList[deviceIter].drive_info.passThroughHacks.useA1SATPassthroughWheneverPossible = true;
+            deviceList[deviceIter].drive_info.passThroughHacks.ataPTHacks.useA1SATPassthroughWheneverPossible = true;
         }
 
         //check for model number match
