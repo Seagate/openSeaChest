002a6597613d0eef97c4d18010f823af4bfda07b
Ameer Hamza <ahamza@ixsystems.com>
2024-02-13 00:08:50 +0500

    feat: Add forceNVME flag
    NVMe devices are detected through the `nvme` prefix, which can be
    unreliable and require explicit specification. This PR adds forceNVME
    flag to force NVMe protocol to communicate with the backend device.
    
    Signed-off-by: Ameer Hamza <ahamza@ixsystems.com>

--- utils/C/openSeaChest/openSeaChest_PowerControl.c
+++ utils/C/openSeaChest/openSeaChest_PowerControl.c
@@ -31,7 +31,7 @@
 //  Global Variables  //
 ////////////////////////
 const char *util_name = "openSeaChest_PowerControl";
-const char *buildVersion = "3.5.0";
+const char *buildVersion = "3.6.0";
 
 ////////////////////////////
 //  functions to declare  //
@@ -961,6 +961,8 @@ int32_t main(int argc, char *argv[])
     }
 
     if ((FORCE_SCSI_FLAG && FORCE_ATA_FLAG)
+	|| (FORCE_SCSI_FLAG && FORCE_NVME_FLAG)
+	|| (FORCE_ATA_FLAG && FORCE_NVME_FLAG)
         || (FORCE_ATA_PIO_FLAG && FORCE_ATA_DMA_FLAG && FORCE_ATA_UDMA_FLAG)
         || (FORCE_ATA_PIO_FLAG && FORCE_ATA_DMA_FLAG)
         || (FORCE_ATA_PIO_FLAG && FORCE_ATA_UDMA_FLAG)
@@ -1248,6 +1250,15 @@ int32_t main(int argc, char *argv[])
                 printf("\tForcing ATA Drive\n");
             }
             deviceList[deviceIter].drive_info.drive_type = ATA_DRIVE;
+        }
+
+        if (FORCE_NVME_FLAG)
+        {
+            if (VERBOSITY_QUIET < toolVerbosity)
+            {
+                printf("\tForcing NVME Drive\n");
+            }
+            deviceList[deviceIter].drive_info.drive_type = NVME_DRIVE;
         }
 
         if (FORCE_ATA_PIO_FLAG)
